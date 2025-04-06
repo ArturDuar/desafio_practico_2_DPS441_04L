a51182bd649ae59747d204e1d2534567e790dce7
Arturo <arturduar60@gmail.com>
2025-04-06 15:42:42 -0600

    feat: create colors file to manage global colors

---
+++ gestion-citas-taller/src/utils/colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+    COLOR_PRIMARY: 'rgb(25,112,0)',
+    COLOR_SECONDARY: 'rgb(189,189,189)',
+    COLOR_TERCIARY: 'rgb(13, 110, 253)',
+    COLOR_WARNING: 'rgb(255, 193, 7)',
+}
+
+export default Colors;
