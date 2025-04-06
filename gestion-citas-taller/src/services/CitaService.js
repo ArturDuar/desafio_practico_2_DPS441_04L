61445a4f8e7796de573f34cd5fda8b3ce1164726
Arturo <arturduar60@gmail.com>
2025-04-06 15:41:28 -0600

    feat: create service file to handle create edite and delete

---
+++ gestion-citas-taller/src/services/CitaService.js
@@ -0,0 +1,79 @@
+import {Alert} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {navegarInicio} from "../utils/validations";
+
+export const obtenerCitas = async () =>{
+    try{
+        
+        const citas = await AsyncStorage.getItem('citas');
+        return citas ? JSON.parse(citas) : [];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const guardarCita = async (citas, setCitas) => {
+        try {
+            await AsyncStorage.setItem('citas', JSON.stringify(citas));
+            const actualizarCitas = await obtenerCitas();
+            setCitas(actualizarCitas);
+        } catch (error) {
+            console.log(error);
+        }
+
+}
+
+export const eliminarCita = (cita, setCitas) => {
+    Alert.alert(
+        "Eliminar",
+        "¿Deseas eliminar esta cita?",
+        [
+            {
+                text: "Cancelar",
+                onPress: () => console.log("Cancelado"),
+                style: "cancel",
+            },
+            {
+                text: "OK",
+                onPress: () => {
+                    obtenerCitas().then((citas) => {
+                        const nuevasCitas = citas.filter((item) => item.id !== cita.id);
+                        guardarCita(nuevasCitas, setCitas);
+                        setCitas(nuevasCitas);
+                    });
+                    console.log(`Cita Eliminada ${cita.id}`);
+
+                },
+            },
+        ]
+    );
+}
+
+export const editarCita = (cita, setCitas, navigation) => {
+    Alert.alert(
+        "Editar",
+        "¿Deseas editar esta cita?",
+        [
+            {
+                text: "Cancelar",
+                onPress: () => console.log("Cancelado"),
+                style: "cancel",
+            },
+            {
+                text: "OK",
+                onPress: () => {
+                    obtenerCitas().then((citas) => {
+                        const nuevasCitas = citas.map((item) => 
+                            item.id === cita.id ? cita : item
+                        );
+                        guardarCita(nuevasCitas, setCitas);
+                        setCitas(nuevasCitas);
+                    });
+                    console.log(`Cita Editada ${cita.id}`);
+                    navegarInicio(navigation);
+
+                },
+            },
+        ]
+    );
+}
